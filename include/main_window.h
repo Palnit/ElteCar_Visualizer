256b4bb125c850c2e4bea9612e10d00e5912f197
Palnit <105753906+Palnit@users.noreply.github.com>
2025-04-20 19:53:37 +0200

    Working camera

--- include/main_window.h
+++ include/main_window.h
@@ -1,12 +1,14 @@
 #ifndef ELTECAR_VISUALIZER_INCLUDE_MAIN_WINDOW_H
 #define ELTECAR_VISUALIZER_INCLUDE_MAIN_WINDOW_H
 
-#include <SDL3/SDL_surface.h>
-#include <vector>
-#include "cartesians.h"
 #include <HUH/Graphics/basic_window.h>
 #include <HUH/Graphics/shader_program.h>
 #include <HUH/Graphics/vertex_array_object.h>
+#include <SDL3/SDL_surface.h>
+#include <vector>
+#include "HUH/Graphics/camera.h"
+#include "HUH/Graphics/texture.h"
+#include "cartesians.h"
 #include "general/SharedMemory/bufferd_reader.h"
 #include "general/SharedMemory/threaded_multi_reader_handler.h"
 #include "lidar_data.h"
@@ -26,12 +28,17 @@ public:
      */
     MainWindow(const char* title, int x, int y, int w, int h, Uint32 flags)
         : BasicWindow(title, x, y, w, h, flags),
-          m_image(nullptr) {}
+          m_image(nullptr),
+          m_camera(glm::vec3(0.0f, 0.0f, -3.0f)) {
+        m_camera.SetAspect(static_cast<float>(m_width)
+                           / static_cast<float>(m_height));
+    }
 
     ~MainWindow() {
         delete m_threaded;
         delete m_csvReader;
         delete m_lidarReader;
+        for (const auto Params : m_textureParams) { delete Params; }
     }
 
     /*!
@@ -44,6 +51,10 @@ public:
      * Implementation of the Render function of the base class
      */
     void Render() override;
+    void KeyboardDown(const SDL_KeyboardEvent& ev) override;
+    void KeyboardUp(const SDL_KeyboardEvent& ev) override;
+    void MouseMove(const SDL_MouseMotionEvent& ev) override;
+    void Update() override;
 
 private:
     SharedMemory::BufferedReader<Cartesians>* m_csvReader;
@@ -53,10 +64,6 @@ private:
     SDL_Surface* m_image2 = nullptr;
     SDL_Surface* m_image3 = nullptr;
     SDL_Surface* m_image4 = nullptr;
-    GLuint tex;
-    GLuint tex2;
-    GLuint tex3;
-    GLuint tex4;
     HUH::VertexArrayObject VAO;
     HUH::VertexArrayObject VAO2;
     HUH::VertexArrayObject VAO3;
@@ -66,9 +73,11 @@ private:
     HUH::VertexBufferObject<float> VBO3;
     HUH::VertexBufferObject<float> VBO4;
     HUH::ElementBufferObject EBO;
+    std::vector<HUH::ITextureParameters*> m_textureParams;
     GLuint vertexShader;
     GLuint fragmentShader;
     HUH::ShaderProgram shaderProgram;
+    HUH::Camera m_camera;
 };
 
 #endif// ELTECAR_VISUALIZER_INCLUDE_MAIN_WINDOW_H
--- src/main_window.cpp
+++ src/main_window.cpp
@@ -1,64 +1,49 @@
 #include "main_window.h"
 #include <SDL3/SDL_surface.h>
 #include <SDL3_image/SDL_image.h>
+#include <glm/ext/matrix_clip_space.hpp>
 #include <vector>
+
 #include "HUH/Graphics/file_handling.h"
+#include "HUH/Graphics/generic_structs.h"
+#include "HUH/Graphics/texture.h"
 #include "cartesians.h"
 #include "general/SharedMemory/bufferd_reader.h"
 #include "general/SharedMemory/threaded_multi_reader_handler.h"
 #include "lidar_data.h"
 
-/// temporary function to read sdl_surface from shm
-/// @param pointer pointer to shared memory
-/// @param size size of shared memory
-/// @return the returned sdl_surface pointer
-SDL_Surface* tmp(void* pointer, int size) {
-    SDL_IOStream* rwops = SDL_IOFromConstMem(pointer, size);
-
-    SDL_Surface* LoadedImg = IMG_Load_IO(rwops, 0);
-
-#if SDL_BYTEORDER == SDL_LIL_ENDIAN
-    SDL_PixelFormat format = SDL_PIXELFORMAT_ABGR8888;
-#else
-    SDL_PixelFormat format = SDL_PIXELFORMAT_RGBA8888;
-#endif
-
-    SDL_Surface* image;
-    image = SDL_ConvertSurface(LoadedImg, format);
-
-    SDL_DestroySurface(LoadedImg);
-    return image;
-}
 /// temporary function to get lidar data array
 /// @param pointer pointer to shared memory
 /// @param size size of shared memory
 /// @return the returned lidar array
-std::vector<LidarData> tmp2(void* pointer, int size) {
+std::vector<LidarData> LidarReader(void* pointer, int size) {
     std::vector<LidarData> output;
-    LidarData* lidarPoiter = (LidarData*) pointer;
+    const auto* LidarPointer = static_cast<LidarData*>(pointer);
     for (int i = 0; i < size / sizeof(LidarData); i++) {
-        output.push_back(*lidarPoiter);
-        lidarPoiter++;
+        output.push_back(*LidarPointer);
+        LidarPointer++;
     }
     return output;
 }
 
 int MainWindow::Init() {
+    m_textureParams.push_back(new HUH::TextureParameters(
+        glTexParameteri, GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR));
+    m_textureParams.emplace_back(new HUH::TextureParameters(
+        glTexParameteri, GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR));
 
     m_threaded = new SharedMemory::ThreadedMultiReaderHandler<SDL_Surface*>(
-        "Images", tmp);
+        "Images", HUH::FileHandling::LoadImageFromMemory);
 
-    m_lidarReader =
-        new SharedMemory::BufferedReader<std::vector<LidarData>>("Lidar", tmp2);
+    m_lidarReader = new SharedMemory::BufferedReader<std::vector<LidarData>>(
+        "Lidar", LidarReader);
 
     m_csvReader = new SharedMemory::BufferedReader<Cartesians>(
-        "Csv", [](void* pointer, int size) { return *(Cartesians*) pointer; });
+        "Csv", [](void* pointer, int size) {
+            return *static_cast<Cartesians*>(pointer);
+        });
 
     //for demonstration only
-    glGenTextures(1, &tex);
-    glGenTextures(1, &tex2);
-    glGenTextures(1, &tex3);
-    glGenTextures(1, &tex4);
     float verts[] = {-1.0f, 0.0f, 0.0f, 0.0f, 1.0f, -1.0f, 1.0f,
                      0.0f,  0.0f, 0.0f, 0.0f, 1.0f, 0.0f,  1.0f,
                      0.0f,  0.0f, 0.0f, 0.0f, 1.0f, 1.0f};
@@ -82,10 +67,10 @@ int MainWindow::Init() {
     EBO.AddElement({2, 3, 1, 3, 0, 1});
 
     vertexShader = HUH::FileHandling::LoadShader(GL_VERTEX_SHADER,
-                                            "shaders/default_vertex.vert");
+                                                 "shaders/default_vertex.vert");
 
-    fragmentShader = HUH::FileHandling::LoadShader(GL_FRAGMENT_SHADER,
-                                              "shaders/default_fragment.frag");
+    fragmentShader = HUH::FileHandling::LoadShader(
+        GL_FRAGMENT_SHADER, "shaders/default_fragment.frag");
 
     shaderProgram.AttachShader(vertexShader);
     shaderProgram.AttachShader(fragmentShader);
@@ -112,9 +97,18 @@ int MainWindow::Init() {
 void MainWindow::Render() {
     glClearColor(0.45f, 0.55f, 0.60f, 1.00f);
     glViewport(0, 0, m_width, m_height);
-    glCullFace(GL_BACK);
+    // glCullFace(GL_BACK);
     glClear(GL_COLOR_BUFFER_BIT);
     shaderProgram.Bind();
+    glm::mat4 model = glm::mat4(1.0f);
+    shaderProgram.SetUniform("model", glUniformMatrix4fv, 1, GL_FALSE,
+                             &model[0][0]);
+    glm::mat4 view = m_camera.GetViewMatrix();
+    shaderProgram.SetUniform("view", glUniformMatrix4fv, 1, GL_FALSE,
+                             &view[0][0]);
+    glm::mat4 projection = m_camera.GetProjectionMatrix();
+    shaderProgram.SetUniform("projection", glUniformMatrix4fv, 1, GL_FALSE,
+                             &projection[0][0]);
     VAO.Bind();
     auto data = m_threaded->ReadMultiMemory();
     if (data.empty()) {
@@ -124,65 +118,70 @@ void MainWindow::Render() {
     }
     bool fail;
     auto Lidar = m_lidarReader->readData(fail);
-    if (Lidar.size() > 0) {
-        std::cout << "Lidar:" << Lidar.size() << std::endl;
-    }
-    auto Cart = m_csvReader->readData(fail);
-    std::cout << "Cartesians ID: " << Cart.ID << " Alt: " << Cart.Alt
-              << std::endl;
+    // if (Lidar.size() > 0) {
+    //     std::cout << "Lidar:" << Lidar.size() << std::endl;
+    // }
+    // auto Cart = m_csvReader->readData(fail);
+    // std::cout << "Cartesians ID: " << Cart.ID << " Alt: " << Cart.Alt
+    //           << std::endl;
 
     //demosntration code only
     if (m_image != nullptr) { SDL_DestroySurface(m_image); }
     m_image = data[0];
 
-    glBindTexture(GL_TEXTURE_2D, tex);
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, m_image->w, m_image->h, 0, GL_RGBA,
-                 GL_UNSIGNED_BYTE, m_image->pixels);
-
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-
+    HUH::Texture2D TestTexture(m_image);
+    TestTexture.SetParameters(m_textureParams);
+    TestTexture.Bind(0);
     glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
+    TestTexture.UnBind();
     VAO.UnBind();
 
     VAO2.Bind();
     if (m_image2 != nullptr) { SDL_DestroySurface(m_image2); }
     m_image2 = data[1];
-    glBindTexture(GL_TEXTURE_2D, tex2);
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, m_image2->w, m_image2->h, 0,
-                 GL_RGBA, GL_UNSIGNED_BYTE, m_image2->pixels);
 
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-
+    HUH::Texture2D TestTexture2(m_image2);
+    TestTexture2.SetParameters(m_textureParams);
+    TestTexture2.Bind(0);
     glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
+    TestTexture2.UnBind();
     VAO2.UnBind();
 
     VAO3.Bind();
     if (m_image3 != nullptr) { SDL_DestroySurface(m_image3); }
     m_image3 = data[2];
-    glBindTexture(GL_TEXTURE_2D, tex3);
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, m_image3->w, m_image3->h, 0,
-                 GL_RGBA, GL_UNSIGNED_BYTE, m_image3->pixels);
 
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-
+    HUH::Texture2D TestTexture3(m_image3);
+    TestTexture3.SetParameters(m_textureParams);
+    TestTexture3.Bind(0);
     glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
+    TestTexture3.UnBind();
     VAO3.UnBind();
 
     VAO4.Bind();
     if (m_image4 != nullptr) { SDL_DestroySurface(m_image4); }
     m_image4 = data[3];
-    glBindTexture(GL_TEXTURE_2D, tex4);
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, m_image4->w, m_image4->h, 0,
-                 GL_RGBA, GL_UNSIGNED_BYTE, m_image4->pixels);
 
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-
+    HUH::Texture2D TestTexture4(m_image4);
+    TestTexture4.SetParameters(m_textureParams);
+    TestTexture4.Bind(0);
     glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
+    TestTexture4.UnBind();
+
     VAO4.UnBind();
 
     shaderProgram.UnBind();
+}
+void MainWindow::KeyboardDown(const SDL_KeyboardEvent& ev) {
+    m_camera.ProcessKeyboardDown(ev);
+}
+void MainWindow::KeyboardUp(const SDL_KeyboardEvent& ev) {
+    m_camera.ProcessKeyboardUp(ev);
+}
+void MainWindow::MouseMove(const SDL_MouseMotionEvent& ev) {
+    m_camera.ProcessMouseMovement(ev);
+}
+void MainWindow::Update() {
+    BasicWindow::Update();
+    m_camera.Update();
 }
